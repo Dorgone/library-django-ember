9991b346a58efa746a5c517722279e584ffe7106
Dorgone <38913668+Dorgone@users.noreply.github.com>
2020-11-18 15:33:20 +0100

    [BASE] Function delete-book

--- frontend/app/routes/book.js
+++ frontend/app/routes/book.js
@@ -5,5 +5,17 @@ export default Route.extend({
     store: service(),
     model(book) {
         return this.get('store').peekRecord('book', book.book_id);
+    },
+    setupController(controller, model) {
+        this._super(controller, model);
+        this.controller.set('confirmingDelete', false);
+    },
+    actions: {
+        delete(book) {
+            book.deleteRecord();
+            book.save().then(() => {
+                this.transitionTo('books');
+            });
+        }
     }
 });
